16292ac7cd9b682edd0a26a348fa2eeced9aae27
Ahmet Selim Çanakçı <73101853+aselimc@users.noreply.github.com>
2022-09-13 01:06:51 +0300

    Time: 4 ms (63.59%), Space: 8.5 MB (37.31%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
-        vector<int> output(nums.size());
-        for (int i=0; i<nums.size(); i++)
-        {
-            int sum = accumulate(nums.begin(), next(nums.begin(), i+1), 0);
-            output[i] = sum;
+    vector<int> runningSum(vector<int> &nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            // Result at index `i` is sum of result at `i-1` and element at `i`.
+            nums[i] += nums[i - 1];
         }
-        return output;
+        return nums;
     }
 };
