414ff1515c4af0f8f63d3f37dcc849a690424eae
Ahmet Selim Çanakçı <73101853+aselimc@users.noreply.github.com>
2022-09-24 21:06:30 +0200

    Time: 7 ms (40.38%), Space: 6.6 MB (81.34%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int n = 0;
+        vector<int> count(128, 0);
+        for (char c : s){
+            if (count[c-'A']==0){
+                count[c-'A']++;
+            }
+            else{
+                count[c-'A']--;
+                n++;
+            }
+        }
+        n *= 2;
+        if (accumulate(count.begin(), count.end(), 0)==0){
+            return n;
+        }
+        else{
+            return n+1;
+        }
+    }
+};
