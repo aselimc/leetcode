10f2ab4395ff207622a2a3c4b5eefdbfcf4b6ec5
Ahmet Selim Çanakçı <73101853+aselimc@users.noreply.github.com>
2023-03-29 14:35:05 +0200

    Time: 26 ms (93.42%), Space: 13.8 MB (92.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -7,8 +7,8 @@ class Solution:
         else:
             first, second, i = 0, 1, 0
             while i < n-1:
-                answer = first + second #1  #2 #3
-                first = second          # 1 #1 #2
-                second = answer         # 1 #2 #3
+                answer = first + second
+                first = second
+                second = answer
                 i += 1
             return answer
